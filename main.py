c1d23cc3c2c60fda1a30caea7547f2001ce70e45
harrygritt <126033604+harrygritt@users.noreply.github.com>
2023-02-27 12:29:45 +1300

    array quantity counter, user order array

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Cheap fish ($4.10 each): Shark, Flounder, Cod, Gurnet, Hoki, and Goldfish.
 #Deluxe Fish ($7.20 each): Snapper, Pink Salmon, Tuna, Smoked Marlin, Pufferfish, Blobfish.
 #If Frozen - discount $1.05 per fish item
-#If Delivery - plus $5.00 to total cost, ask name, address, amnd phone no.
+#If Delivery - plus $5.00 to total cost, ask name, address, and phone no.
 #If Pick up - ask name and phone no.
 #MAX 7 fish per type, e.g. 7 cod and 7 flounder is acceptable. display nice error if invalid entry.
 #At end of order, display names of fish (with price), total cost (display delivery cost), customer name, if cooked or frozen, if chips (plus price), and if for delivery (address and phonoe no.)
@@ -17,19 +17,22 @@ deluxe_fish = [
     [7.20, 7.20, 7.20, 7.20, 7.20, 7.20]
 ]
 
-
+user_order = [
+    [],
+    []
+]
 
 index_for_name = 0
 index_for_price = 1
 invalid_entry = "Please select a valid option"
 
 
-def fish_catagory_function():
+def fish_catagory():
     while True:
         try:
-            divider_function()
-            print_array_function(["Deluxe","Cheaper"])
-            divider_function()
+            divider()
+            print_array(["Deluxe","Cheaper"])
+            divider()
             fish_catagory = int(input("Would you like a deluxe or a cheaper fish? "))
         except ValueError:
             print(invalid_entry)
@@ -37,14 +40,14 @@ def fish_catagory_function():
         
         match fish_catagory:
             case 1:
-                fish_order_function(deluxe_fish)
+                fish_order(deluxe_fish)
             case 2:
-                fish_order_function(cheap_fish)
+                fish_order(cheap_fish)
             case _:
                 print(invalid_entry)
 
 
-def input_checking_function(prompt: str, array: list):
+def input_checking(prompt: str, array: list):
     while True:
         try:
             input_index = int(input(prompt))
@@ -57,26 +60,40 @@ def input_checking_function(prompt: str, array: list):
         else:
             return input_index
 
-def fish_order_function(fish_array: list):
-    divider_function()
-    print_array_function(fish_array[index_for_name])
-    divider_function()
-    fish_index = input_checking_function("What fish woukld you like? ", fish_array[index_for_name])
-    fish_amount = input_checking_function("How many of this fish would you like? ", range(7))
+def fish_order(fish_array: list):
+    divider()
+    print_array(fish_array[index_for_name])
+    divider()
     
-    item_price = (fish_array[index_for_price][fish_index - 1]) * fish_amount
+    fish_index = input_checking("What fish woukld you like? ", fish_array[index_for_name])
+    fish_remaining = array_quantity_counter(fish_array[fish_index], user_order[index_for_name])
+    print(fish_remaining)
+    fish_amount = input_checking("How many of this fish would you like? ", range(7))
+    item_price = (fish_array[index_for_price][fish_index - 1]) 
+    price_string = "%.2f" % (item_price * fish_amount)
 
-    price_string = "%.2f" % item_price
-    print("Your choice: " + fish_array[index_for_name][fish_index - 1] + " x" + str(fish_amount) + ", Price: $" + price_string)
+    print("Your choice: " + fish_array[index_for_name][fish_index - 1] + " x" + str(fish_amount) + ", Price: $"
+         + price_string)
+    for each in range(fish_amount):
+        user_order[index_for_name].append(fish_array[index_for_name][fish_index - 1])
+        user_order[index_for_price].append(item_price)
+    
+
+def array_quantity_counter(key, array: list):
+    quantity = 0
+    for item in array:
+        if item == key:
+            quantity += 1
+    return quantity
 
 
-def print_array_function(items: list):
+def print_array(items: list):
     for index in range(len(items)):
         print(f"{index + 1}) {items[index]}")
 
 
-def divider_function():
+def divider():
     print(20 * "-")
 
 if __name__ == "__main__":
-    fish_catagory_function()
+    fish_catagory()
