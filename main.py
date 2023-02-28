e8ab0014eabe6abaa7fa875ac96007edefde0437
harrygritt <126033604+harrygritt@users.noreply.github.com>
2023-02-28 13:32:43 +1300

    fixed amount of certain fish counter

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def fish_catagory():
     while True:
         try:
             divider()
-            print_array(["Deluxe","Cheaper"])
+            print_array(["Deluxe","Cheaper","Finish Order"])
             divider()
             fish_catagory = int(input("Would you like a deluxe or a cheaper fish? "))
         except ValueError:
@@ -43,6 +43,8 @@ def fish_catagory():
                 fish_order(deluxe_fish)
             case 2:
                 fish_order(cheap_fish)
+            case 3:
+                print("PLACE HOLDER")
             case _:
                 print(invalid_entry)
 
@@ -60,22 +62,32 @@ def input_checking(prompt: str, array: list):
         else:
             return input_index
 
+#Function that orders fish
 def fish_order(fish_array: list):
+    #Print menu
     divider()
     print_array(fish_array[index_for_name])
     divider()
-    
+
+    #Get index of fish from user input based on fish array, and store it
     fish_index = input_checking("What fish woukld you like? ", fish_array[index_for_name])
-    fish_remaining = array_quantity_counter(fish_array[fish_index], user_order[index_for_name])
-    print(fish_remaining)
-    fish_amount = input_checking("How many of this fish would you like? ", range(7))
+    fish_name = fish_array[index_for_name][fish_index - 1]
+    
+    #Check number of chosen fish and calculate the amount the user can order
+    fish_remaining = 7 - array_quantity_counter(fish_name, user_order[index_for_name])
+    fish_amount = input_checking("How many of this fish would you like? ", range(fish_remaining))
+
+    #Get price from array and format as string
     item_price = (fish_array[index_for_price][fish_index - 1]) 
     price_string = "%.2f" % (item_price * fish_amount)
 
-    print("Your choice: " + fish_array[index_for_name][fish_index - 1] + " x" + str(fish_amount) + ", Price: $"
+    #Print ordered fish and price
+    print("Your choice: " + fish_name + " x" + str(fish_amount) + ", Price: $"
          + price_string)
+
+    #Put ordered fish in user's order array
     for each in range(fish_amount):
-        user_order[index_for_name].append(fish_array[index_for_name][fish_index - 1])
+        user_order[index_for_name].append(fish_name)
         user_order[index_for_price].append(item_price)
     
 
